7511bc1ffdd0bb30f88fc4ac305bd06ad7b17ee5
haoychen3 <114067151+haoychen3@users.noreply.github.com>
2023-03-19 22:11:38 +0800

    Update function.py

--- function.py
+++ function.py
@@ -3,14 +3,12 @@ import random
 import torch
 import numpy as np
 
-
 def setup_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
-
 
 def copy_codes(trainpath1,trainpath2,trainpath3,trainpath4, path1,path2,path3,path4):
     shutil.copyfile(trainpath1, path1)
